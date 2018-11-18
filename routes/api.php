07904550997be9cc2aa94bbe19f89bc90e055406
murphy-warugaki <s.nishimoto@hassyadai.com>
2018-11-19 00:01:47 +0900

    add apply

--- routes/api.php
+++ routes/api.php
@@ -2,17 +2,7 @@
 
 use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::resource('attendances', 'Api\AttendancesController')->only(['index', 'show', 'store']);
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::put('attendances/{attendance}/report', 'Api\AttendancesController@update');
+
