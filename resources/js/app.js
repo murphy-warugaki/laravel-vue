38bf1f810addb971a0cd729d5fa909df027f7db2
murphy-warugaki <s.nishimoto@hassyadai.com>
2018-11-19 13:25:33 +0900

    finished

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,15 +1,30 @@
+//import Vue from 'vue'
+//import VueRouter from 'vue-router'
 
 require('./bootstrap');
 
 window.Vue = require('vue');
 
-
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
-
+// Vue.component('example-component', require('./components/ExampleComponent.vue'));
 Vue.component('paginate', require('vuejs-paginate'));
 Vue.component('attendances', require('./components/Attendances.vue'));
 Vue.component('attendance-form', require('./components/AttendanceForm.vue'));
+Vue.component('router-view', require('./components/Welcome.vue'));
+
+/*
+Vue.use(VueRouter)
+ const router = new VueRouter({
+	mode: 'history',
+	rutes: [
+		{
+			path: '/welcome',
+			component: require('./components/Welcome.vue')
+		}
+	]
+ });
+*/
 
 const app = new Vue({
+//	router,
     el: '#app'
 });
